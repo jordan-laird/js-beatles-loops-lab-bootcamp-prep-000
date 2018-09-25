93e4bd03200006207115a76e52b410f46d663ca3
Jordan Laird <jordanmack.laird@gmail.com>
2018-09-25 05:22:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay(musicians, instruments){
   var arr = []
   for (var i = 0; i <= 3; i++) {
     if (musicians[i] === instruments) {
-      arr.push("${i} plays ${instruments}")
+      arr.push("${musicians} plays ${instruments}")
     }
   }
 }  
